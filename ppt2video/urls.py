03ef7770e6b2e41adc8bcc6b7f3e6c4f8116595a
Mohit Chawla <98903945+Mohit1345@users.noreply.github.com>
2023-02-10 19:53:42 +0530

    Update urls.py

--- ppt2video/urls.py
+++ ppt2video/urls.py
@@ -24,5 +24,7 @@ urlpatterns = [
     path("/download", views.download_file, name="download_file"),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
